ff96653a9d068ec45826d17075986cd121b3e289
Prayoch rujira <prayoch@clockupstudio.com>
2023-08-17 22:22:06 +0700

    Added test cases for exclude_vat

--- exclude_vat.py
+++ exclude_vat.py
@@ -3,5 +3,5 @@ vat = 7
 def vat_exclude(price, vat):
     return price * (vat/(100 + vat))
 
-def exclude_vat(item_list):
-    return [ vat_exclude(item["price"], vat) for item in item_list ]
+def exclude_vat(item_list, whitelist = set()):
+    return [ 0 if whitelist.intersection(item["type"]) else vat_exclude(item["price"], vat) for item in item_list ]
--- test_vat_exclude.py
+++ test_vat_exclude.py
@@ -5,10 +5,54 @@ def test_empty_item_should_return_empty():
     result = exclude_vat([])
     assert result == []
 
-def test_one_item_should_get_one_vat():
-    result = exclude_vat([{
-        "title": "Python so stress",
-        "price": 100,
-        "type": { "book" }
-    }])
+
+def test_one_item_should_get_one_extracted_vat():
+    result = exclude_vat([
+        make_item("Python so stress", 100, { "book" })
+    ])
     assert result == [ 6.5420560747663545 ]
+
+
+def test_two_item_should_get_two_extracted_vat():
+    result = exclude_vat([
+        make_item("Python so stress", 100, { "book" }),
+        make_item("Python so stress", 100, { "book" })
+    ])
+    assert result == [ 6.5420560747663545, 6.5420560747663545 ]
+
+
+def test_one_exclude_item_should_get_one_zero_vat():
+    result = exclude_vat([
+        make_item("Python so stress", 100, { "book" })
+    ], { "book" })
+    assert result == [ 0 ]
+
+
+def test_two_excludes_item_should_get_two_zero_vat():
+    result = exclude_vat([
+        make_item("Python so stress", 100, { "book" }),
+        make_item("Python so stress", 100, { "book" })
+    ], { "book" })
+    assert result == [ 0, 0 ]
+
+
+def test_two_excludes_item_should_get_two_zero_vat():
+    result = exclude_vat([
+        make_item("Python so stress", 100, { "book" }),
+        make_item("Python so stress", 100, { "book" })
+    ], { "book" })
+    assert result == [ 0, 0 ]
+
+def test_two_items_with_one_exclude_item_should_get_one_vat_and_zero_vat():
+    result = exclude_vat([
+        make_item("Python so stress", 100, { "book" }),
+        make_item("Dried mango", 100, { "food" })
+    ], { "book" })
+    assert result == [ 0, 6.5420560747663545 ]
+
+def make_item(title, price, type):
+    return {
+        "title": title,
+        "price": price,
+        "type": type
+    }
